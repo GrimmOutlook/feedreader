8e448eba72322a9a9c6a7b809987752c971f9072
GrimmOutlook <grimmoutlook87@gmail.com>
2016-01-16 18:07:42 -0500

    Added feeds to iindex & created index view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  resources :feeds do
+    member do
+      resources :entries, only: [:index, :show]
+    end
+  end
+
   resources :feeds
 
   root 'feeds#index'
